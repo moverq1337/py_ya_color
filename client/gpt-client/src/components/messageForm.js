14c58c37275f7daab2ecd97a6b82650d7d6214ed
Георгий Церетьян <moverq@MacBook-Air-Georgij.local>
2024-04-27 12:57:13 +0300

    First commit

---
+++ client/gpt-client/src/components/messageForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const MessageForm = () => {
+ const [word, setWord] = useState('');
+ const [colors, setColors] = useState([]);
+
+ const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://127.0.0.1:8000/ask_gpt/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ word }),
+      });
+      if (!response.ok) {
+        throw new Error('Ошибка при отправке сообщения');
+      }
+      const data = await response.json();
+      console.log(data);
+      setColors(data.result);
+      setWord('');
+    } catch (error) {
+      console.error('Ошибка:', error);
+    }
+ };
+
+ const applyColors = () => {
+   return colors.map((color, index) => (
+     <div key={index} style={{ backgroundColor: color, height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#fff', fontSize: '24px', width: "100%" }}>
+       Цвет {index + 1}
+     </div>
+   ));
+ };
+
+ return (
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flexGrow: 1 ,zIndex:"999", position:"absolute", top: "-200px", left: "0", bottom: "0", right: "0"}}>
+        <label htmlFor="word" style={{ marginBottom: '10px' }}></label>
+        <input
+          type="text"
+          id="word"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          style={{ padding: '8px', marginBottom: '10px', width: '200px' }}
+        />
+        <button type="submit" style={{ padding: '8px 16px' }}>Отправить</button>
+      </form>
+      <div style={{display:"flex", zIndex:"0"}}>
+        {applyColors()}
+      </div>
+    </div>
+ );
+};
+
+export default MessageForm;
